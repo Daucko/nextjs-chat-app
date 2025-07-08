4e3fb1f926440f3495224e893e8f250dc4e15221
Dauda <dauda.a.tijani@gmail.com>
2025-07-08 10:31:38 +0100

    Second commit

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@/components/ui/button';
 
-export default function Home() {
+export default function User() {
   // return <Button variant={'destructive'}>Hello World!</Button>;
-  return <Button>Hello World!</Button>;
+  return <h1>This is the user page</h1>;
 }
