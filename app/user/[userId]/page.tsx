ef2005a47308fb09c3f5dad9f03af7e06b8bffe7
Dauda <dauda.a.tijani@gmail.com>
2025-07-08 10:46:54 +0100

    Third commit

---
+++ app/user/[userId]/page.tsx
@@ -0,0 +1,4 @@
+export default function User() {
+  // return <Button variant={'destructive'}>Hello World!</Button>;
+  return <p>User details</p>;
+}
---
+++ app/user/page.tsx
@@ -0,0 +1,4 @@
+export default function User() {
+  // return <Button variant={'destructive'}>Hello World!</Button>;
+  return <h1>User page</h1>;
+}
