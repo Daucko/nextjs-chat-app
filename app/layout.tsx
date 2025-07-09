5a7ec191158c57761c5a7076201e3209a033e9a1
Dauda <dauda.a.tijani@gmail.com>
2025-07-09 17:04:34 +0100

    Added 'app/sign-in' route

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import ConvexClientProvider from '@/providers/ConvexClientProvider';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -27,8 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        This is being rendered in the layout
-        {children}
+        <ConvexClientProvider>{children}</ConvexClientProvider>
       </body>
     </html>
   );
---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: 'https://adapting-tapir-20.clerk.accounts.dev',
+      applicationID: 'convex',
+    },
+  ],
+};
