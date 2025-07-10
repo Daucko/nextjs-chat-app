ad92d3199fc17f5df41f44c3c6ef4207df84e143
Dauda <dauda.a.tijani@gmail.com>
2025-07-10 12:42:46 +0100

    Added 'app/hook'

--- components/shared/sidebar/SidebarWrapper.tsx
+++ components/shared/sidebar/SidebarWrapper.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
-type Props = {};
+type Props = React.PropsWithChildren<{}>;
 
-const SidebarWrapper = (props: Props) => {
-  return <div>SidebarWrapper</div>;
+const SidebarWrapper = ({ children }: Props) => {
+  return (
+    <div className="size-full p-4 flex flex-cols lg:flex-row gap-4">
+      <main className="h-[calc(100%-80px)] lg:size-full flex gap-4">
+        {children}
+      </main>
+    </div>
+  );
 };
 
 export default SidebarWrapper;
---
+++ hooks/useConversation.tsx
@@ -0,0 +1,5 @@
+import { useParams } from 'next/navigation';
+
+export const useConversation = () => {
+  const params = useParams();
+};
