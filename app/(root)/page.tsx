66ab44013ff61a73254a77d70579aa7ec2eb3bc7
Dauda <dauda.a.tijani@gmail.com>
2025-07-10 12:02:32 +0100

    Added 'app/root' folder.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,9 +3,10 @@ import { SignInButton, UserButton } from '@clerk/nextjs';
 export default function Home() {
   // return <Button variant={'destructive'}>Hello World!</Button>;
   return (
-    <div>
-      <UserButton />
-      <SignInButton mode="modal" />
-    </div>
+    // <div>
+    //   <UserButton />
+    //   <SignInButton mode="modal" />
+    // </div>
+    null
   );
 }
