025c80cd73a6eae0a907ece49cdf922e58653e89
Dauda <dauda.a.tijani@gmail.com>
2025-07-11 11:39:13 +0100

    Added 'app/components/shared/sidebar/nav/DesktopNav.tsx'

--- components/shared/sidebar/SidebarWrapper.tsx
+++ components/shared/sidebar/SidebarWrapper.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import DesktopNav from './nav/DesktopNav';
 
 type Props = React.PropsWithChildren<{}>;
 
 const SidebarWrapper = ({ children }: Props) => {
   return (
-    <div className="size-full p-4 flex flex-cols lg:flex-row gap-4">
+    <div className="size-full p-4 flex flex-col lg:flex-row gap-4">
+      <DesktopNav />
       <main className="h-[calc(100%-80px)] lg:size-full flex gap-4">
         {children}
       </main>
