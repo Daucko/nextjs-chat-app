cb1c2f742b9978f6b3268eb44727457eb868aec9
Dauda <dauda.a.tijani@gmail.com>
2025-07-11 12:23:41 +0100

    Updated 'app/root/friends/page.tsx'

--- app/(root)/friends/page.tsx
+++ app/(root)/friends/page.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import ItemList from '../../../components/shared/item-list/ItemList';
 
 type Props = {};
 
 const FriendsPage = (props: Props) => {
-  return <div>FriendsPage</div>;
+  return (
+    <div>
+      <ItemList title="Friends">Friends Page</ItemList>
+    </div>
+  );
 };
 
 export default FriendsPage;
--- components/shared/item-list/ItemList.tsx
+++ components/shared/item-list/ItemList.tsx
@@ -6,8 +6,18 @@ type Props = React.PropsWithChildren<{
   action?: React.ReactNode;
 }>;
 
-const ItemList = ({ children, title, action }: Props) => {
-  return <Card className="size-full lg:flex-none lg:w-80 p-2"></Card>;
+const ItemList = ({ children, title, action: Action }: Props) => {
+  return (
+    <Card className="size-full lg:flex-none lg:w-80 p-2">
+      <div className="mb-4 flex items-center justify-between">
+        <h1 className="text-2xl font-semibold tracking-tight">{title}</h1>
+        {Action ? Action : null}
+      </div>
+      <div className="size-full flex flex-col items-center justify-start gap-2">
+        {children}
+      </div>
+    </Card>
+  );
 };
 
 export default ItemList;
