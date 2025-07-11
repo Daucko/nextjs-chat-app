376fa49b80fbd4eaf57f3d2e463844a6eecc963d
Dauda <dauda.a.tijani@gmail.com>
2025-07-11 12:01:57 +0100

    Added 'app/components/shared/item-list/ItemList.tsx'

--- components/shared/item-list/ItemList.tsx
+++ components/shared/item-list/ItemList.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Card } from '../../ui/card';
 
-type Props = {};
+type Props = React.PropsWithChildren<{
+  title: string;
+  action?: React.ReactNode;
+}>;
 
-const ItemList = (props: Props) => {
-  return <div>ItemList</div>;
+const ItemList = ({ children, title, action }: Props) => {
+  return <Card className="size-full lg:flex-none lg:w-80 p-2"></Card>;
 };
 
 export default ItemList;
